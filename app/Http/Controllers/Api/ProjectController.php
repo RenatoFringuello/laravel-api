c52e17ddb7de4b2d17a71abc1e734dbd3fc97e64
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-02 19:53:13 +0100

    change pagination

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -16,7 +16,7 @@ class ProjectController extends Controller
     public function index()
     {
         //
-        $projects = Project::with(['user', 'type', 'technologies'])->paginate(10);
+        $projects = Project::with(['user', 'type', 'technologies'])->paginate(12);
         return response()->json([
             'success' => true,
             'result'  => $projects
