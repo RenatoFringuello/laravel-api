73d26e14a2377982c28f4454c5565d0085fbd38b
RenatoFringuello <renatofringuello@hotmail.it>
2023-03-07 13:17:25 +0100

    add role and role_user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UsersSeeder::class,
+            RoleSeeder::class,
+            RoleUserSeeder::class,
             TypesSeeder::class,
             ProjectsSeeder::class,
             TechnologySeeder::class,
